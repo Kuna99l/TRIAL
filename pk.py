cacec1b6fa3e2a0d32afafa837a9108ec936fc29
kunal kumar <kunalk12395@gmail.com>
2024-05-26 01:04:24 +0530

    first commit

---
+++ pk.py
@@ -0,0 +1,6 @@
+
+num = int(input('enter a number'))
+i=1
+while i<11:
+    print(num,'X',i,num*i)
+    i+=1
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("kunal kumar");
+totalPrice = 45;
+No_of_items = 50;
+isfollow = true;
+console.log(isfollow);
